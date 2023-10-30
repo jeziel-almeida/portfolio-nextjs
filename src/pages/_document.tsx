f812ed151a655621edca5c397938a443775f85c1
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-10-29 22:17:55 -0300

    feat: add page projects

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="pt-br">
+    <Html lang="pt-br" className='scroll-smooth'>
       <Head>
         <link rel="icon" href="/favicon.png" />
       </Head>
