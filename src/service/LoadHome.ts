9684a0cf16fb2b724aa06f0a7bdaf3ffc2e883c8
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-11-05 17:08:24 -0300

    Update home URL in LoadHome service

--- src/service/LoadHome.ts
+++ src/service/LoadHome.ts
@@ -1,5 +1,5 @@
 export const loadHome = async () => {
-    const res = await fetch('https://gist.githubusercontent.com/jeziel-almeida/dcf88339d5cfe0b840f6abca4dcd1ed4/raw/2b107a005c8c8587337941982fde427b0789fd4f/home.json');
+    const res = await fetch(process.env.NEXT_PUBLIC_HOME_URL ?? "");
     const home = await res.json();
   
     return home;
